990279afd3e322435edc2cae5b90d56c303db7ee
Erdong Chen <erdongchen@M1-Pro-MBP.lan>
2023-04-24 17:11:49 -0700

    correct file naming

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import "./style.css";
 import ReactDOM from "react-dom/client";
-import App from "./app";
+import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
